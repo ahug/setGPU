4fed64f7a4992fbadd67f7a40924e0649f5e2da8
Andreas Hug <andi.hug@gmail.com>
2019-04-24 10:51:50 +0200

    ignores failures (so that it can also be used locally without GPUs)

--- setGPU.py
+++ setGPU.py
@@ -2,13 +2,17 @@ import os
 import gpustat
 import random
 
-stats = gpustat.GPUStatCollection.new_query()
-ids = map(lambda gpu: int(gpu.entry['index']), stats)
-ratios = map(lambda gpu: float(gpu.entry['memory.used'])/float(gpu.entry['memory.total']), stats)
-pairs = list(zip(ids, ratios))
-random.shuffle(pairs)
-bestGPU = min(pairs, key=lambda x: x[1])[0]
 
-print("setGPU: Setting GPU to: {}".format(bestGPU))
-os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
-os.environ['CUDA_VISIBLE_DEVICES'] = str(bestGPU)
+try:
+    stats = gpustat.GPUStatCollection.new_query()
+    ids = map(lambda gpu: int(gpu.entry['index']), stats)
+    ratios = map(lambda gpu: float(gpu.entry['memory.used'])/float(gpu.entry['memory.total']), stats)
+    pairs = list(zip(ids, ratios))
+    random.shuffle(pairs)
+    bestGPU = min(pairs, key=lambda x: x[1])[0]
+
+    print("setGPU: Setting GPU to: {}".format(bestGPU))
+    os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(bestGPU)
+except Exception as e:
+    print("setGPU failed. Maybe no GPUs available?")
